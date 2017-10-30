7114c35d3f7eaccef7cef23ed989e966fc7517dc
Otavio Augusto <tavioalves@gmail.com>
2017-10-29 21:13:16 -0300

    Adição de mais funcionalidades

--- pixel-it.js
+++ pixel-it.js
@@ -1,4 +1,6 @@
 var currentColor = '#fff';
+var lastColor = '#fff';
+var currentTool = null;
 var mouseDown = false;
 
 $(document).ready(function() {
@@ -18,10 +20,30 @@ $(document).ready(function() {
 	$('.color-box').click(function() {
 		var colorClicked = $(this).css('background-color');
 		currentColor = colorClicked;
+		lastColor = currentColor;
 
 		$('.current-color-box').css('background-color', currentColor);	
 
 		console.log("Color chosen was: ", colorClicked);
+	});
+
+
+	$('.tool-box').click(function() {
+		currentTool = $(this).get(0).id;
+		console.log("Tool chosen was: ", currentTool);
+
+		$('.current-tool').empty();
+		$('.current-tool').append("<i class='fa fa-"+currentTool+" fa-2x' aria-hidden='true'></i>");
+
+		if(currentTool === 'eraser') {
+			currentColor = '#d8d8d8';
+			$('.current-color-box').css('background-color', currentColor);
+		} else if (currentTool === 'pencil') {
+			currentColor = lastColor;
+			$('.current-color-box').css('background-color', lastColor);
+		}
+
+
 	});
 
 });
